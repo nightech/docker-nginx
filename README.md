dbf168517786017511898009fe7647f849a3c248
tchiotludo <tchiot.ludo@gmail.com>
2017-02-08 13:18:45 +0100

    CLIENT_MAX_BODY_SIZE vars

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## Environment Variables
 * `WORKER_PROCESSES` : "1"
 * `WORKER_CONNECTIONS` : "1024"
+* `CLIENT_MAX_BODY_SIZE` : "8m"
--- overlay/usr/local/bin/docker-entrypoint.sh
+++ overlay/usr/local/bin/docker-entrypoint.sh
@@ -6,11 +6,15 @@ set -e
 declare -A ENV_CONFIG=()
 ENV_CONFIG[WORKER_PROCESSES]=${WORKER_PROCESSES:-"1"}
 ENV_CONFIG[WORKER_CONNECTIONS]=${WORKER_CONNECTIONS:-"1024"}
+ENV_CONFIG[CLIENT_MAX_BODY_SIZE]=${CLIENT_MAX_BODY_SIZE:-"8m"}
 
-for FILE in `find  /etc/nginx/nginx.conf -type f -name *.conf`;  do
-    for KEY in "${!ENV_CONFIG[@]}"; do
-        sed -i.bak "s!\%"$KEY"\%!"${ENV_CONFIG[$KEY]}"!g" $FILE
-    done
+for FILE in `find  /etc/nginx/ -type f -name *.conf`;  do
+    if [ -w "$FILE" ]
+    then
+        for KEY in "${!ENV_CONFIG[@]}"; do
+            sed -i.bak "s!\%"$KEY"\%!"${ENV_CONFIG[$KEY]}"!g" $FILE
+        done
+    fi
 done
 
 find  /etc/nginx -type f -name *.bak -delete
